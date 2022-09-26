47d11f4bd1b5a51c0c0b33872f76b91559d12867
Talox11 <taloboti@gmail.com>
2022-09-25 21:03:37 -0500

    env variables for uri db connection

--- src/database/database.js
+++ src/database/database.js
@@ -6,10 +6,21 @@ const DB_HOST = process.env.DB_HOST
 const DB_PASSWORD = process.env.DB_PASSWORD
 const DB_DATABASE = process.env.DB_DATABASE
 const DB_PORT = process.env.DB_PORT
-const URI = `postgres://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`;
-//postgres://nwuurhotgsjaap:1c19817aa8761680d7f8bc5e69e46008cc069ce689e4e37b6750fb857c3420ac@ec2-3-214-2-141.compute-1.amazonaws.com:5432/d76djgu2rdjgnb
 
-export const sequelize = new Sequelize(URI);
+export const sequelize = new Sequelize({
+    database: DB_DATABASE,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    host: DB_HOST,
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            require: true, // This will help you. But you will see nwe error
+            rejectUnauthorized: false // This line will fix new error
+        }
+    },
+});
 
 sequelize
     .authenticate()
