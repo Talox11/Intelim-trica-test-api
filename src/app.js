fdf5c4090b433e7896e194410a90f6bb45d82b8a
Talox11 <taloboti@gmail.com>
2022-09-25 20:17:17 -0500

    port added to prod

--- src/app.js
+++ src/app.js
@@ -9,8 +9,8 @@ app.use(express.json());
 
 // Routes
 
-app.listen(4000,() => {
-    console.log(`listening on port ${4000}`);
+app.listen(port,() => {
+    console.log(`listening on port ${port}`);
 })
 
 
