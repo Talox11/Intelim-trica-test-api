ac204784fe2c0cb2ebab537464b28e4e3cbab043
Talox11 <taloboti@gmail.com>
2022-09-25 20:04:28 -0500

    file to start server changed due issues

--- src/app.js
+++ src/app.js
@@ -8,6 +8,16 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 // Routes
+
+app.listen(4000,() => {
+    console.log(`listening on port ${4000}`);
+})
+
+
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API For Melp '})
+})
+
 app.use(restaurantRoutes);
 
 export default app;
